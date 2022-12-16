09109776d2f6a9db6aa7c5b2ada5f04afcd21cc4
Hee ju Park <69663388+heejucherish@users.noreply.github.com>
2022-12-16 18:39:05 +0900

    [Silver I] Title: RGB거리, Time: 140 ms, Memory: 116452 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split()))for i in range(n)]
+dp = [[-1 for i in range(3)] for j in range(1010)]
+
+def recur(cur, prv):
+    global ans
+    if cur == n :
+        return 0
+
+    if dp[cur][prv] != -1:
+        return dp[cur][prv] 
+    ret = 1<<30
+    for i in range(3):
+        if i == prv:
+            continue
+        
+        ret = min(ret,recur(cur+1, i )+ arr[cur][i])
+    
+    dp[cur][prv] = ret
+    return ret
+
+
+
+print(recur(0,-1))
