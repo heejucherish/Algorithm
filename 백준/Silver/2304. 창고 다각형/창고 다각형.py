1c1cf93c696c95a0426b1f72db02fd1798c62c1c
Hee ju Park <69663388+heejucherish@users.noreply.github.com>
2022-12-25 22:24:15 +0900

    [Silver II] Title: 창고 다각형, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -0,0 +1,33 @@
+
+import sys
+
+input = sys.stdin.readline
+
+
+# 막대기둥
+n = int(input())
+
+arr = [0 for i in range(1010)]
+
+for i in range(n):
+    a,b = map(int, input().split())
+
+    arr[a] = b
+
+prefix = [0 for i in range(1010)]
+prefix[0]  = arr[0]
+for i in range(1, 1010):
+    prefix[i]  = max(prefix[i-1], arr[i])
+
+suffix = [0 for i in range(1010)]
+
+for i in range(1009)[::-1]:
+    suffix[i] = max(suffix[i+1], arr[i])
+
+ans = 0
+
+for i in range(1010):
+    ans += min(prefix[i], suffix[i])
+
+
+print(ans)
