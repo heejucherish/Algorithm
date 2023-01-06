5b70ea0b8ec4e4824b71671fb0b3c1b1c9b77cb0
Hee ju Park <69663388+heejucherish@users.noreply.github.com>
2023-01-07 00:17:05 +0900

    [Gold V] Title: 숫자고르기, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+values = [0]
+for _ in range(N):
+    values.append(int(input()))
+answer = set()
+
+def dfs(index, first, second):
+    first.add(index)
+    second.add(values[index])
+    if values[index] in first:
+        if first == second:
+            answer.update(first)
+        return
+    return dfs(values[index], first, second)
+
+for i in range(1, N+1):
+    if i not in answer:
+        dfs(i, set(), set())
+
+print(len(answer))
+for num in sorted(list(answer)):
+    print(num)
